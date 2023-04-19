70bc49039dd4f5904542e95a832f55b3b9d7bd00
José Antonio Quintero Maya <a10joseqm@ATEIS.LOCAL>
2023-04-19 19:58:41 +0200

    Creo la clase principal en la vista

---
+++ src/main/java/org/example/Principal.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.controller.CarController;
+import org.example.view.Dialog;
+import org.example.view.Menu;
+
+/**
+ * @Author: José Antonio Quintero Maya
+ */
+
+public class Principal {
+    public static void main(String[] args) {
+      Menu menu = new Menu();
+  //menu.getStarted();
+        menu.carManager();
+
+
+
+
+    }
+}
